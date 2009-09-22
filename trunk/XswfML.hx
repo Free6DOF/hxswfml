d225c4f88f0a29ba795ac20e8cd989688b35f5f1
adnez7 <adnez7@d7e250fa-a51f-11de-b195-ab392a2a73f5>
2009-09-22 13:14:16 +0000

    reading width and height from images files. Fixed mp3. Rewrote doc.txt

--- trunk/XswfML.hx
+++ trunk/XswfML.hx
@@ -49,9 +49,11 @@ class HXswfML
 	private var currentTagNode:Xml;
 	private var swfBytesOutput:BytesOutput;
 	private var swfWriter:format.swf.Writer;
+	private var bitmapIds:Array<Array<Int>>;//[[width, height]]
 	
 	public function new(fileIn:String, fileOut:String)
 	{
+		bitmapIds = new Array();
 		//get xml
 		this.fileIn = fileIn;
 		var xml:Xml = Xml.parse(File.getContent(fileIn));
@@ -148,27 +150,37 @@ class HXswfML
 		var file = _getString('file',"");
 		checkAttS(file, 'file');
 		var bytes = neko.io.File.getBytes(file);
+		storeWidthHeight(id, file, bytes);
 		return TBitsJPEG(id, JDJPEG2(bytes));
 	}
 	private function defineShape()
 	{
 		var id = _getInt('id',null);
 		checkAtt(id, 'id');
-		var width = _getInt('width',null);
-		checkAtt(width, 'width');
-		var height = _getInt('height',null);
-		checkAtt(height, 'height');
 		var bounds;
 		var shapeWithStyle;
 		if(currentTagNode.exists('bitmapId'))
 		{
 			var bitmapId = _getInt('bitmapId');
-			bounds = {left :0*20, right : width*20, top:0*20,  bottom : height*20}
+			var width = bitmapIds[bitmapId][0];
+			var height = bitmapIds[bitmapId][1];
+			var scaleX = _getFloat('scaleX',1.0);
+			var scaleY = _getFloat('scaleY',1.0);
+			var scale = (scaleX==1.0 && scaleY==1.0)?{x:scaleX*20, y:scaleY*20}:{x:scaleX*20, y:scaleY*20};
+			var rotate0 = _getFloat('rotate0',0.0);
+			var rotate1 = _getFloat('rotate1',0.0);
+			var rotate = (rotate0==0.0 && rotate1==0.0)?{rs0:rotate0*20, rs1:rotate1*20}:{rs0:rotate0*20, rs1:rotate1*20};
+			var x = _getInt('x',0);
+			var y = _getInt('y',0);
+			var translate = (x==0 && y==0)?{x:x*20, y:y*20}:{x:x*20, y:y*20}
+			var repeat:Bool = _getBool('repeat',false);
+			var smooth:Bool = _getBool('smooth',false);
+			bounds = {left :x*20, right : x*20 + width*20, top:y*20,  bottom : y*20 + height*20}
 			shapeWithStyle = 
 			{
 				fillStyles:
 				[
-					FSBitmap(1, {scale:{x:0.0, y:0.0}, rotate:{rs0:0.0, rs1:0.0}, translate:{x:0, y:0}}, false, false)
+					FSBitmap(1, {scale:scale, rotate:rotate, translate:translate}, repeat, smooth)
 				],
 				lineStyles:
 				[
@@ -178,56 +190,37 @@ class HXswfML
 				[	
 					SHRChange(
 					{	
-						moveTo:{dx:width*20,dy:0*20}, 
+						moveTo:{dx:x*20+width*20,dy:y*20}, 
 						fillStyle0:{idx:1}, 
 						fillStyle1:null, 
 						lineStyle:null, 
 						newStyles:null
 					}), 
-					SHREdge(0, height*20),
-					SHREdge(-width*20, 0), 
-					SHREdge(0, -height*20), 
-					SHREdge(width*20, 0), 
+					SHREdge(x*20, y*20+height*20), 
+					SHREdge(x*20-width*20, y*20), 
+					SHREdge(x*20, y*20-height*20), 
+					SHREdge(x*20+width*20, y*20), 
 					SHREnd 
 				]
 			}
 		}
 		else
 		{
-			var width = _getInt('width');
+			var width = _getInt('width', null);
 			checkAtt(width, 'width');
-			var height = _getInt('height');
+			var height = _getInt('height', null);
 			checkAtt(height, 'height');
 			var fillColor = _getInt('fillColor',0x000000);
 			var lineColor = _getInt('lineColor',0x000000);
-			var lineWidth = _getInt('lineWidth',1);
-			bounds =
-			{
-				left:0*20, 
-				right:width*20, 
-				top:0*20, 
-				bottom:height*20
-			};
+			var lineWidth = _getInt('lineWidth',0);
+			var x = _getInt('x',0);
+			var y = _getInt('y',0);
+			bounds ={ left:x*20, right:x*20+width*20, top:y*20, bottom:y*20 + height*20};
 			var fillStyles = 
 			[
-				FSSolid(
-				{
-					r:(fillColor & 0xff0000) >> 16, 
-					g:(fillColor & 0x00ff00) >> 8, 
-					b:(fillColor & 0x0000ff)
-				})
-			];
-			var lineStyles = 
-			[
-				{	
-					width:lineWidth*20, 
-					data:LSRGB(
-					{
-						r:(lineColor & 0xff0000) >> 16, g:(lineColor & 0x00ff00) >> 8, b:lineColor & 0x0000ff
-					})
-				}
-			];
-			
+				FSSolid({r:(fillColor & 0xff0000) >> 16, g:(fillColor & 0x00ff00) >> 8, b:(fillColor & 0x0000ff)})
+			] ;
+			var lineStyles = (lineWidth==0)?[]:[{width:lineWidth*20, data:LSRGB({r:(lineColor & 0xff0000) >> 16, g:(lineColor & 0x00ff00) >> 8, b:lineColor & 0x0000ff})}];
 			shapeWithStyle = 
 			{
 				fillStyles:fillStyles,				
@@ -236,26 +229,16 @@ class HXswfML
 				[
 					SHRChange(
 					{
-						moveTo:
-						{
-							dx:width*20,
-							dy:0*20
-						},
-						fillStyle0:
-						{
-							idx:1
-						}, 
+						moveTo:{dx:x*20+width*20, dy:y*20},
+						fillStyle0:{idx:1}, 
 						fillStyle1:null, 
-						lineStyle:
-						{
-							idx:1
-						}, 
+						lineStyle:{idx:1}, 
 						newStyles:null
 					}), 
-					SHREdge(0, height*20), 
-					SHREdge(-width*20, 0), 
-					SHREdge(0, -height*20), 
-					SHREdge(width*20, 0), 
+					SHREdge(x*20, y*20+height*20), 
+					SHREdge(x*20-width*20, y*20), 
+					SHREdge(x*20, y*20-height*20), 
+					SHREdge(x*20+width*20, y*20), 
 					SHREnd 
 				]
 			}	
@@ -318,24 +301,7 @@ class HXswfML
 					up:up,
 					id:id,
 					depth:depth,
-					matrix:
-					{
-						scale:
-						{
-							x:scaleX, 
-							y:scaleY
-						}, 
-						rotate:
-						{
-							rs0:rs0, 
-							rs1:rs1
-						}, 
-						translate:
-						{
-							x:x*20, 
-							y:y*20
-						}
-					}
+					matrix:{scale:{x:scaleX, y:scaleY}, rotate:{rs0:rs0, rs1:rs1}, translate:{x:x*20, y:y*20}}
 				});
 		}
 		return TDefineButton2(id, buttonRecords);
@@ -377,8 +343,8 @@ class HXswfML
 				case DualChannel: true;
 				case Mono:false;
 			},
-			//samples : haxe.Int32.ofInt(mp3.sampleCount),
-			samples : haxe.Int32.ofInt(0),				
+			samples : haxe.Int32.ofInt(mp3.sampleCount),
+			//samples : haxe.Int32.ofInt(0),				
 			data : SDMp3(0, dataBytesOutput.getBytes())
 		});
 	}
@@ -628,7 +594,59 @@ class HXswfML
 	{
 		return TEnd;
 	}	
+//helper for defineShape for DefineBitsJPEG2
+	private function storeWidthHeight(id:Int, fileName:String, _bytes:Bytes):Void
+	{
+		var extension = fileName.substr(fileName.lastIndexOf('.')+1);
+		var height=0;
+		var width=0;
+		var bytes = new BytesInput(_bytes);
+		if(extension.toLowerCase()=='jpg' || extension.toLowerCase()=='jpeg')
+		{
+			if (bytes.readByte() != 255 || bytes.readByte() != 216)
+			{
+				throw "SOI (Start Of Image) marker 0xff 0xd8 missing";
+			}
+			var marker:Int;
+			var len:Int;
+			while (bytes.readByte() == 255) 
+			{
+				marker = bytes.readByte();
+				len = bytes.readByte() << 8 | bytes.readByte();
 
+				if (marker == 192)
+				{
+					bytes.readByte();
+					height = bytes.readByte() << 8 | bytes.readByte();
+					width = bytes.readByte() << 8 | bytes.readByte();
+					break;
+				}
+				bytes.read(len - 2);
+			}
+		}
+		else if(extension.toLowerCase()=='png' )
+		{
+			bytes.bigEndian=true;
+			bytes.readInt32();//0x89504e47 SOI
+			bytes.readInt32();//0x0D0A1A0A
+			bytes.readInt32();//0x0000000D THDR
+			bytes.readInt32();//0x49484452
+			//width = haxe.Int32.toInt(bytes.readInt32());
+			//height = haxe.Int32.toInt(bytes.readInt32());
+			width = bytes.readUInt30();
+			height = bytes.readUInt30();
+		}
+		else if(extension.toLowerCase()=='gif' )
+		{
+			bytes.bigEndian=false;
+			bytes.readInt32();//0x47 0x49 0x46 0x38 
+			bytes.readUInt16();//0x39 0x61
+			width = bytes.readUInt16();
+			height = bytes.readUInt16();
+		}
+		bitmapIds[id]=[width, height];
+		
+	}
 	//helpers for reading xml attributes and converting to correct datatype and/or default value
 	private function _getInt(att:String, ?defaultValue:Int):Int
 	{
