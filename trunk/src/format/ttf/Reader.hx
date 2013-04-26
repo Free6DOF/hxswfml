56bef655087334012509d7a818da559d06f7284c
adnez7@gmail.com <adnez7@gmail.com@d7e250fa-a51f-11de-b195-ab392a2a73f5>
2013-04-26 13:16:54 +0000

    -use hhead table when OS/2 table is not present -support for reading font name in Macintosh fonts (platformId 1)

--- trunk/src/format/ttf/Reader.hx
+++ trunk/src/format/ttf/Reader.hx
@@ -52,10 +52,11 @@ class Reader
 		
 		//var postData = readPostTable(tablesHash.get("post"));
 		//tables.push(TPost(postData));
-		
-		var os2Data = readOS2Table(tablesHash.get("OS_2"));
-		tables.push(TOS2(os2Data));
-		
+		if(tablesHash.exists("OS_2"))
+		{
+			var os2Data = readOS2Table(tablesHash.get("OS_2"));
+			tables.push(TOS2(os2Data));
+		}
 		var nameData = readNameTable(tablesHash.get("_name"));
 		tables.push(TName(nameData));
 
@@ -769,7 +770,6 @@ class Reader
 		var _format = input.readUInt16();//0
 		var count = input.readUInt16();
 		var stringOffset = input.readUInt16();
-
 		var nameRecords:Array<NameRecord> = new Array();
 		for(i in 0...count)
 		{
@@ -788,7 +788,7 @@ class Reader
 		var fontNameRecord=null;
 		for(i in 0...count)
 		{
-			if(nameRecords[i].nameID==4 && (nameRecords[i].platformId==3 || nameRecords[i].platformId==0))
+			if(nameRecords[i].nameID==4 && (nameRecords[i].platformId==0 || nameRecords[i].platformId==1 || nameRecords[i].platformId==3))
 			{
 				fontNameRecord = nameRecords[i];
 				break;
@@ -799,36 +799,17 @@ class Reader
 		else
 		{
 			input.read(fontNameRecord.offset);
-			for (i in 0...Std.int(fontNameRecord.length/2))
-				fontNameRecord.record+=String.fromCharCode(input.readUInt16());
+			switch(fontNameRecord.platformId)
+			{
+				case 0, 3:// Unicode (big-endian)// Microsoft encoding, Unicode
+					for (i in 0...Std.int(fontNameRecord.length/2))
+						fontNameRecord.record+=String.fromCharCode(input.readUInt16());
+				case 1, 2:// Macintosh encoding, ASCII// ISO encoding, ASCII
+					for (i in 0...fontNameRecord.length)
+						fontNameRecord.record+=String.fromCharCode(input.readByte());
+			}
 		}
 		fontName = fontNameRecord.record;
-		/*
-		//offsets don't always match with length and there is overlapping. for now we only search the font name (above).
-		var lastOffset = -1;
-		for(i in 0...count)
-		{
-			var nameRecord = nameRecords[i];
-			var stringBuf = new StringBuf();
-			
-			if(nameRecords[i].offset != lastOffset)
-			{
-				lastOffset = nameRecords[i].offset;
-				switch(nameRecord.platformId)
-				{
-					case 0, 3:// Unicode (big-endian)// Microsoft encoding, Unicode
-						for (i in 0...Std.int(nameRecord.length/2))
-							stringBuf.add(String.fromCharCode(input.readUInt16()));
-
-					case 1, 2:// Macintosh encoding, ASCII// ISO encoding, ASCII
-						for (i in 0...nameRecord.length)
-						stringBuf.add(String.fromCharCode(input.readByte()));
-				}
-			}
-			nameRecord.record = stringBuf.toString();
-			//trace(nameRecord.record);
-		}
-		*/
 		return nameRecords;
 	}
 	//0S2 (compatibility) table
